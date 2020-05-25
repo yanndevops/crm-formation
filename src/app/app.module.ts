8a2dad1d87d6078effb8c674048b39e306ebfbad
tirard-gatel <yann.tirard-gatel@atos.net>
2020-05-25 15:59:47 +0200

    install librairi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { AppComponent } from './app.component';
     LoginModule,
     TextModule,
     IconsModule,
-    CoreModule
+    CoreModule,
+    NgbModule.forRoot(),
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
