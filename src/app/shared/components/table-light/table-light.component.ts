1337054b2e29271ede820c4d57c91e449e957781
tirard-gatel <yann.tirard-gatel@atos.net>
2020-05-27 14:50:22 +0200

    encaps

--- src/app/shared/components/table-light/table-light.component.ts
+++ src/app/shared/components/table-light/table-light.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Order } from '../../models/order';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-table-light',
   templateUrl: './table-light.component.html',
-  styleUrls: ['./table-light.component.scss']
+  styleUrls: ['./table-light.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TableLightComponent implements OnInit {
   @Input() headers: string[];
@@ -15,8 +15,6 @@ export class TableLightComponent implements OnInit {
   }
 
 }
-
-
 
 
 
